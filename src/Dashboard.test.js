ab092cc95de7049597a9a4cf5bdd251176068e69
thiagoalgo <thiagoalgo@gmail.com>
2021-02-28 15:50:14 -0300

    Ajustdo arquivos de teste e css

--- src/Dashboard.test.js
+++ src/Dashboard.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Dashboard from './Dashboard';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+it('deve renderizar o componente sem erros', () => {
+  render(<Dashboard />);
+  const linkElement = screen.getByText(/Dashboard/i);
   expect(linkElement).toBeInTheDocument();
 });
